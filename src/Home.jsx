b5490556a48636b962630e1fc4b2d86a8a6edda5
Montassar Bousselmi <mbousselmi024@gmail.com>
2025-02-22 19:38:55 +0100

    new changes

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,21 +10,24 @@ function Home(){
                 </div>
             </div>
             <div className='partners'>
-                <div className='partner'></div>
+                <div className='partner'>
+                    
+                </div>
                 
 
 
             </div>
             <div className='about'>
 
+                <div>
+                    <img src="src\components\logo.png" alt="" />
+                    <p>ISAMM MICROSOFT CLUB, Is your gateway to a world of technology, innovation, and career growth. We are a vibrant community of passionate tech enthusiasts and aspiring professionals dedicated to exploring the vast realm of Microsoft technologies. Through engaging workshops, exciting competitions, and personalized career development services, we offer services for learning, collaboration, and skill enhancement. Join us on this exciting journey where dreams transform into digital realities. Embrace the future of technology with ISAMM Microsoft Club where we are driven by passion!</p>
+                </div>
+
+
 
 
             </div>
-
-
-
-
-
         </section>
     )
 }
